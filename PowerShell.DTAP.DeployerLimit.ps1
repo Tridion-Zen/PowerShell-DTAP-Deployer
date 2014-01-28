c2e2052df81e9102c7475079f2dd4cb97e523b30
Tridion.Zen <Tridion-Zen@users.noreply.github.com>
2014-01-28 11:12:51 +0100

    Update PowerShell.DTAP.DeployerLimit.ps1

--- PowerShell.DTAP.DeployerLimit.ps1
+++ PowerShell.DTAP.DeployerLimit.ps1
@@ -3,31 +3,37 @@
 $ErrorActionPreference = "SilentlyContinue"
 cls
 #
-$servers = @("SERVEUR_UN", "SERVEUR-DEUX")
+$servers = @("SERVEUR_UNE", "SERVEUR-DEUX")
 #
 $startTime = get-date
 write-host "Started $($startTime)"
 #
 foreach ($server in $servers) {   
-    write-host "Server $server"
+    write-host "Server: $server"
     #
-    write-host "Searching in \ABC for cd_deployer_conf.xml"
-    $files = get-childitem -path "\\$server\c$\ABC\" -recurse -force -include "cd_deployer_conf.xml"
-    write-host "Files $files" -foregroundcolor gray
+    write-host "Search in: \data\www\tridion\2011  for: cd_deployer_conf.xml"
+    $files = get-childitem -path "\\$server\f$\data\www\tridion\2011\" -recurse -force -include "cd_deployer_conf.xml"
+    write-host "Files found: $files" -foregroundcolor gray
     foreach ($file in $files) {
-        write-host "File $file" -foregroundcolor yellow
+        write-host "File: $file" -foregroundcolor yellow
         [xml] $conf = get-content $file
-        $conf.deployer.httpsreceiver
+        write-host $conf.deployer.httpsreceiver.name         " " -nonewline
+        write-host $conf.deployer.httpsreceiver.maxsize.name " " -nonewline
+        write-host $conf.deployer.httpsreceiver.maxsize 
     } 
     #
-    write-host "Searching in \DEF for web.config"
-    $files = get-childitem -path "\\$server\c$\DEF\" -recurse -force -include "web.config"
-    write-host "Files $files" -foregroundcolor gray
+    write-host "Search in: \data\www\tridion\2011  for: web.config"
+    $files = get-childitem -path "\\$server\f$\data\www\tridion\2011\" -recurse -force -include "web.config"
+    write-host "Files found: $files" -foregroundcolor gray
     foreach ($file in $files) {
-        write-host "File $file" -foregroundcolor yellow
+        write-host "File: $file" -foregroundcolor yellow
         [xml] $conf = get-content $file
-        $conf.configuration.'system.web'.httpruntime
-        $conf.configuration.'system.webserver'.security.requestfiltering.requestlimits
+        write-host $conf.configuration.'system.web'.httpruntime.name                  " " -nonewline
+        write-host $conf.configuration.'system.web'.httpruntime.maxrequestlength.name " " -nonewline 
+        write-host $conf.configuration.'system.web'.httpruntime.maxrequestlength
+        write-host $conf.configuration.'system.webserver'.security.requestfiltering.requestlimits.name                         " " -nonewline
+        write-host $conf.configuration.'system.webserver'.security.requestfiltering.requestlimits.maxallowedcontentlength.name " " -nonewline
+        write-host $conf.configuration.'system.webserver'.security.requestfiltering.requestlimits.maxallowedcontentlength
     }
 }
 write-host "Finished $(get-date)"
